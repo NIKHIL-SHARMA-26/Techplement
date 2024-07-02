b120e7a6119739d35b8d38cc9fe24f59f695f7ec
Nikhil Sharma <100345481+NIKHIL-SHARMA-26@users.noreply.github.com>
2024-07-02 19:01:27 +0530

    Update server.js

--- week1-tasks/server.js
+++ week1-tasks/server.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const {connectDB} = require('./DataBase/quoteDB') 
+const cors = require('cors');
+
 
 const app = express()
 const port = process.env.PORT;
