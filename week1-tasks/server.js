905cab41e209951946197226c876bbf715f7b155
Nikhil Sharma <100345481+NIKHIL-SHARMA-26@users.noreply.github.com>
2024-07-02 19:10:40 +0530

    Update server.js

--- week1-tasks/server.js
+++ week1-tasks/server.js
@@ -8,8 +8,8 @@ const cors = require('cors');
 const app = express()
 const port = process.env.PORT;
 
-app.use(express.json())
 app.use(cors());
+app.use(express.json())
 
 const quoteRouter = require('./routes/quote')
 app.use('/api/quote',quoteRouter)
